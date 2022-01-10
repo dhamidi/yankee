53a4a26529b65c6a7df29f831d175bbd8fea5ec2
Dario Hamidi <dario.hamidi@gmail.com>
2022-01-10 11:24:48 +0200

    feat: add matcher for docker containers

--- lib/default.sh
+++ lib/default.sh
@@ -4,17 +4,62 @@ subcommand_default() {
   local -a words=($input)
 
   case "${words[0]:-file}" in
-    git)  _default_git_branch "$input" "$cursor";;
-    ssh)  _default_ssh_host "$input" "$cursor";;
-    kill) _default_process_id "$input" "$cursor";;
-    *)    _default_file "$input" "$cursor";;
+    git)   _default_git "$input" "$cursor";;
+    cargo) _default_cargo "$input" "$cursor";;
+    ssh)   _default_ssh_host "$input" "$cursor";;
+    docker|docker-compose) _default_docker_container "$input" "$cursor";;
+    man)   _default_man "$input" "$cursor";;
+    kill)  _default_process_id "$input" "$cursor";;
+    *)     _default_file "$input" "$cursor";;
   esac
+}
+
+_default_git() {
+  if [[ "${1%% }" == git ]]; then
+    _default_git_subcommand "$1" "$2"
+  else
+    _default_git_branch "$1" "$2"
+  fi
+}
+
+_default_cargo() {
+  if [[ "${1%% }" == cargo ]]; then
+    _default_cargo_subcommand "$1" "$2"
+  fi
+}
+
+_default_cargo_subcommand() {
+  local -a cargo_subcommand=()
+  cargo_subcommand=(
+    $(cargo --list |
+        awk 'NR > 1 {print $1}' |
+        fzf --preview-window=right:70% --preview='cargo help {}')
+  )
+ 
+  if [[ -n "${cargo_subcommand[0]:-}" ]]; then
+    subcommand insert " ${cargo_subcommand[0]}" "$1" "$2"
+  fi
+}
+
+_default_git_subcommand() {
+  local -a git_subcommand=($(git help -a |
+    sed -ne '/^[[:space:]]/s/^[[:space:]]\+//p' |
+    awk '{print $1}' |
+    fzf --preview='man git-{}' --preview-window='right:70%'))
+
+  subcommand insert "${git_subcommand[0]:-}" "$1" "$2"
 }
 
 _default_git_branch() {
   local branch=$(git branch | awk '{print $NF}' | fzf)
 
   subcommand insert " $branch " "$1" "$2"
+}
+
+_default_man() {
+  local manpage=$(whatis -r '.' | fzf)
+  local -a words=($manpage)
+  subcommand insert " ${words[0]:-}" "$1" "$2"
 }
 
 _default_ssh_host() {
@@ -26,6 +71,16 @@ _default_ssh_host() {
 _default_process_id() {
   local -a pid=($(ps -a -o pid,command | tail -n +2 | fzf))
   subcommand insert "${pid[0]}" "$1" "$2"
+}
+
+_default_docker_container() {
+  local containers=$(docker ps --format '{{.Names}}')
+  if [[ $(wc -l <<<"$containers") -le 1 ]]; then 
+    containers=$(docker ps -a --format '{{.Names}}')
+  fi
+  local container=$(fzf <<<"$containers")
+
+  subcommand insert "${container}" "$1" "$2"
 }
 
 _default_file() {
